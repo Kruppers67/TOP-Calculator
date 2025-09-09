257cfebcd82c999eee7c023865949e8fb492d32b
Kruppers67 <lyladanelleroberts@gmail.com>
2025-09-09 13:25:12 -0600

    Basic math logic and adding btns.

---
+++ index.js
@@ -0,0 +1,21 @@
+function addNums(num1, num2) {
+    return num1 + num2;
+}
+
+function subNums(num1, num2) {
+    return num1 - num2;
+}
+function divideNums(num1, num2) {
+    return num1 / num2;
+}
+function multiplyNums(num1, num2) {
+    return num1 * num2;
+}
+console.log(addNums(2,5));
+console.log(subNums(2,5));
+console.log(divideNums(6,3));
+console.log(multiplyNums(10, 2));
+
+function userInput(num1, num2, operator) {
+    
+}
